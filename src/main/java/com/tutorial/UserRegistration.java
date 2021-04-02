12da7ef597e399e1d14e82c44da71495c656c07e
Kajal <kajalpatil158@gmail.com>
2021-04-02 15:01:25 +0530

    [Kajal Patil]Added: Validation For Email

--- src/main/java/com/tutorial/UserRegistration.java
+++ src/main/java/com/tutorial/UserRegistration.java
@@ -7,11 +7,28 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	Scanner scanner = new Scanner(System.in);
 	String firstName;
+	String lastName;
+	String email;
 
+	// Get User Input For Validation
 	public String getFirstName() {
-		System.out.println("enter your first name");
+		System.out.println("Enter your first name");
 		firstName = scanner.nextLine();
 		return firstName;
+	}
+
+	// Get User Input For Validation
+	public String getLastName() {
+		System.out.println("Enter your last name");
+		lastName = scanner.nextLine();
+		return lastName;
+	}
+
+	//
+	public String getEmail() {
+		System.out.println("Enter Email -");
+		email = scanner.nextLine();
+		return email;
 	}
 
 	// Checking valid first name
@@ -20,20 +37,49 @@ public class UserRegistration {
 		Matcher matcher = pattern.matcher(firstName);
 		boolean matches = matcher.find();
 		if (matches) {
-			System.out.println("name is valid");
+			System.out.println("First Name is valid");
 		} else {
-			System.out.println("name is Invalid");
+			System.out.println("First Name is Invalid");
 		}
 		return matches;
 	}
 
+	// Validation for Last Name
+	public boolean validateLastName(String lastName) {
+		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]");
+		Matcher matcher = pattern.matcher(lastName);
+		boolean matches = matcher.find();
+		if (matches) {
+			System.out.println("Last Name Is Valid");
+		} else {
+			System.out.println("Last Name Is Invalid");
+		}
+		return matches;
+	}
+	// Checking valid Email
+	public boolean validateEmail(String email) {
+		Pattern pattern = Pattern.compile(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Matcher matcher = pattern.matcher(email);
+		boolean matches = matcher.find();
+		if (matches) {
+			System.out.println("Email Is Valid");
+		} else {
+			System.out.println("Email Is Invalid");
+		}
+		return matches;
+	}
+	//Call All Methods Here..
 	public static void main(String[] args) {
 		{
 			System.out.println("well Come In User Registration....");
 			UserRegistration userRegistration = new UserRegistration();
 			String firstName = userRegistration.getFirstName();
 			userRegistration.validateName(firstName);
+			String lastName = userRegistration.getLastName();
+			userRegistration.validateLastName(lastName);
+			String email = userRegistration.getEmail();
+			userRegistration.validateEmail(email);
 		}
-
 	}
 }
--- src/test/java/com/tutorial/UserRegistrationTest.java
+++ src/test/java/com/tutorial/UserRegistrationTest.java
@@ -10,11 +10,25 @@ public class UserRegistrationTest {
 	public UserRegistrationTest() {
 		userRegistration = new UserRegistration();
 	}
-
+	//Unit Test For First Name
 	@Test
 	public void validateName() {
 		String name = "Kajal";
 		boolean result = userRegistration.validateName(name);
+		Assert.assertTrue(result);
+	}
+	//Unit Test For Last Name
+	@Test
+	public void validateLastName() {
+		String lastname = "Patil";
+		boolean result = userRegistration.validateLastName(lastname);
+		Assert.assertTrue(result);
+	}
+	//Unit Test For Email
+	@Test
+	public void validateEmail() {
+		String email = "Kajalpatil158@gmail.com";
+		boolean result = userRegistration.validateEmail(email);
 		Assert.assertTrue(result);
 	}
 }
